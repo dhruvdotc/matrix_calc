c1b470dae47cb6dd4d72828fe8a816a5173e9b1d
dhruvdotc <dhruv.roych@gmail.com>
2025-07-05 21:57:48 +0800

    Prototyped with matrix addition

--- prototype.py
+++ prototype.py
@@ -1,7 +1,10 @@
 """
-This is the calculator file, wherein functions will be added using branch workflows
-I will implement regular and matrix calculator seperately via operations
+This is a file where I was experimenting ways to make the calculator.
+I prototyped creation of seperate functions for each operation but realized an 
+interactive if statement would make things much more user-friendly and streamlined.
+
 """
+import numpy as np
 
 def float_add():
     """Adds two float values from user input and prints the sum."""
@@ -13,3 +16,14 @@ def float_add():
 
 """ Testing """
 float_add()
+
+def matrix_add():
+    print("2x2 matrix addition!")
+    val1 = input("Enter four values for first matrix, each seperated by a single space: ").split()
+    mat1 = np.array([ [float(val1[0]), float(val1[1])], [float(val1[2]) , float(val1[3])] ])
+    val2 = input("Enter four values for second matrix, each seperated by a single space: ").split()
+    mat2 = np.array([ [float(val2[0]), float(val2[1])], [float(val2[2]) , float(val2[3])] ])
+    result = np.add(mat1, mat2)
+    print(f"The matrix addition of {mat1} and {mat2} is {result}")
+
+
